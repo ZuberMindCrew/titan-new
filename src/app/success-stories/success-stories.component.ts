64ebd4bc624c8f659ea69fe0dd5fcc86bd627b78
ZuberMindCrew <mohammed.zuber@mindcrewtech.com>
2025-05-01 16:11:34 +0530

    update

--- src/app/success-stories/success-stories.component.ts
+++ src/app/success-stories/success-stories.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-success-stories',
-  templateUrl: './success-stories.component.html',
-  styleUrls: ['./success-stories.component.scss']
+  selector: "app-success-stories",
+  templateUrl: "./success-stories.component.html",
+  styleUrls: ["./success-stories.component.scss"],
 })
 export class SuccessStoriesComponent {
-
+  statistics = [
+    {
+      percentage: "30%",
+      description: "Faster GTM Growth",
+      variant: "gray",
+    },
+    {
+      percentage: "25%",
+      description: "Boost In Productivity",
+      subtext: "(3 mo)",
+      variant: "beige",
+    },
+    {
+      percentage: "42%",
+      description: "Increase In Performance",
+      subtext: "(6 mo)",
+      variant: "gray",
+    },
+    {
+      percentage: "36%",
+      description: "Avg Revenue Growth Through Up-Skilling",
+      variant: "beige",
+    },
+    {
+      percentage: "22-63%",
+      description: "Increase Total Compensation",
+      variant: "gray",
+    },
+  ]
 }
