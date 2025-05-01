331bfca1be88ec40d8cb344e635d2663ae190c72
ZuberMindCrew <mohammed.zuber@mindcrewtech.com>
2025-05-01 16:30:50 +0530

    update

--- src/app/services-section/services-section.component.ts
+++ src/app/services-section/services-section.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-services-section',
-  templateUrl: './services-section.component.html',
-  styleUrls: ['./services-section.component.scss']
+  selector: "app-services-section",
+  templateUrl: "./services-section.component.html",
+  styleUrls: ["./services-section.component.scss"],
 })
-export class ServicesSectionComponent {
+export class ServicesSectionComponent implements OnInit {
+  ngOnInit() {
+    // Preload images for better performance
+    const preloadImages = ["assets/images/conference-table.png", "assets/images/executives-meeting.png"]
 
+    preloadImages.forEach((src) => {
+      const img = new Image()
+      img.src = src
+    })
+  }
 }
